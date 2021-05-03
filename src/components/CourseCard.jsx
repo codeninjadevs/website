ad36863bf9f3763467cc6fca3961db1c6db51127
Shakil Ahmed <shakilahmmeed@gmail.com>
2021-05-04 03:03:01 +0600

    fix: small fix about css building

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -4,11 +4,11 @@ import Link from "next/link";
 export default function CourseCard({ title, slug, thumbnail }) {
 	return (
 		<>
-			<div className="shadow rounded-lg p-3">
+			<div className="shadow rounded-lg p-3" key={slug}>
 				<Link href={"course/" + slug}>
 					<a>
 						<img src={thumbnail} alt={title} className="rounded" />
-						<h3 className="mt-4 font-semibold text-sm inline-block">
+						<h3 className="mt-4 font-semibold inline-block">
 							{title}
 							<span className="bg-red-600 px-2 py-0.5 ml-2 rounded-full text-xs font-semibold text-white">
 								আসন্ন
