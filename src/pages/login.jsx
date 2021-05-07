9ebdbead15c38d2871689e44e7fc1e72563aecd6
Shakil Ahmed <shakilahmmeed@gmail.com>
2021-05-08 05:32:45 +0600

    ui: login page ui is done

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -3,7 +3,30 @@ import React from "react";
 export default function Roadmap() {
 	return (
 		<div>
-			<h1>এটা লগিন পেজ</h1>
+			<div
+				className="flex items-center justify-center"
+				style={{ height: "calc(100vh - 100px)" }}
+			>
+				<div className="shadow w-96 p-5 rounded">
+					<h2 className="text-center font-bold text-xl">Access your account</h2>
+					<div className="mt-5">
+						<a
+							href="#"
+							className="py-2.5 px-5 block rounded bg-gray-800 text-center text-white hover:text-white hover:shadow-md"
+						>
+							<i className="fab fa-github mr-3"></i>
+							Login with GitHub
+						</a>
+						<a
+							href="#"
+							className="mt-2 py-2.5 px-5 block rounded bg-blue-500 text-center text-white hover:text-white hover:shadow-md"
+						>
+							<i className="fab fa-google mr-3"></i>
+							Login with Google
+						</a>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
